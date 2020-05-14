1aa8958cd82bcea8bf01a0f7876caf33a12ff2c5
StyxOo <l.rothenhaeusler@gmail.com>
2020-05-14 17:44:52 +0200

    Added code comments

--- UI/api_wrapper/__init__.py
+++ UI/api_wrapper/__init__.py
@@ -1,10 +1,17 @@
+"""
+This script is part of the api_wrapper package. It is executed when the package is imported.
+ It checks if there is a config file present and creates the default one if not.
+"""
+
 import os
 import json
 
 
+# Path to config file
 _config_path = './config.json'
 
 
+# Creates default config file
 def _create_config():
     data = {'ip': '192.0.0.1', 'port': '1880', 'url': '/fail', 'save': False}
 
@@ -17,5 +24,6 @@ if not os.path.isfile(_config_path):
     print("No configuration file found. Creating default configuration file")
     _create_config()
 
+# Read config and store contents in _config variable for use in the package
 with open(_config_path) as config_file:
     _config = json.load(config_file)
--- UI/api_wrapper/wrapper.py
+++ UI/api_wrapper/wrapper.py
@@ -1,3 +1,8 @@
+"""
+This is the main module of the api_wrapper package. It is responsible for creating and sending HTTP requests to the
+robotic hands controller.
+"""
+
 import os
 import json
 import requests
@@ -5,6 +10,7 @@ from . import _config
 from . import _config_path
 
 
+# Saves current configuration to config file
 def _write_config():
     if os.path.isfile(_config_path):
         os.remove(_config_path)
@@ -13,39 +19,45 @@ def _write_config():
         json.dump(_config)
 
 
+# Set the ip of the current configuration. If it should be saved in the config.json, pass store=True
 def set_ip(ip, store=False):
     _config['ip'] = ip
     if store:
         _write_config()
 
 
+# Set the port of the current configuration. If it should be saved in the config.json, pass store=True
 def set_port(port, store=False):
     _config['port'] = port
     if store:
         _write_config()
 
 
+# Set the url of the current configuration. If it should be saved in the config.json, pass store=True
 def set_url(url, store=False):
     _config['url'] = url
     if store:
         _write_config()
 
 
+# Should http or https be used for the connection? If it should be saved in the config.json, pass store=True
 def set_use_save_connection(use_save_connection, store=False):
     _config['save'] = use_save_connection
     if store:
         _write_config()
 
 
+# Internal function which create the requests url from the current configuration.
 def _create_request_url():
-    # url = 'http'
-    # if _config['save']:
-    #     url += 's'
+    url = 'http'
+    if _config['save']:
+        url += 's'
     url = '{}:{}{}'.format(_config['ip'], _config['port'], _config['url'])
     print(url)
     return url
 
 
+# Sends a HTTP request. Creates query parameters from given finger inputs.
 def _send_request(f1=None, f2=None, f3=None, f4=None, f5=None):
     payload = {}
     if f1 is not None:
@@ -63,6 +75,7 @@ def _send_request(f1=None, f2=None, f3=None, f4=None, f5=None):
     print("Request returned status: {}".format(r.status_code))
 
 
+# Clamps a percent value between 0 and 100. Is used to make sure query parameters do not extend percent values.
 def _clamp_percent(value):
     if value < 0:
         print("Less than 0 percent specified for extension. Clamping to 0")
@@ -73,31 +86,37 @@ def _clamp_percent(value):
     return value
 
 
+# Call this function with a percent value between 0 and 100 to move only the first finger
 def move_finger1(percent):
     percent = _clamp_percent(percent)
     _send_request(f1=percent)
 
 
+# Call this function with a percent value between 0 and 100 to move only the second finger
 def move_finger2(percent):
     percent = _clamp_percent(percent)
     _send_request(f2=percent)
 
 
+# Call this function with a percent value between 0 and 100 to move only the third finger
 def move_finger3(percent):
     percent = _clamp_percent(percent)
     _send_request(f3=percent)
 
 
+# Call this function with a percent value between 0 and 100 to move only the fourth finger
 def move_finger4(percent):
     percent = _clamp_percent(percent)
     _send_request(f4=percent)
 
 
+# Call this function with a percent value between 0 and 100 to move only the fifth finger
 def move_finger5(percent):
     percent = _clamp_percent(percent)
     _send_request(f5=percent)
 
 
+# Call this with percents between 0 and 100 for each finger. Makes the whole hand move.
 def move_hand(f1, f2, f3, f4, f5):
     f1 = _clamp_percent(f1)
     f2 = _clamp_percent(f2)
@@ -107,6 +126,7 @@ def move_hand(f1, f2, f3, f4, f5):
     _send_request(f1=f1, f2=f2, f3=f3, f4=f4, f5=f5)
 
 
+# This function can make the whole hand move. Parameters are optional, so the fingers to move can be chosen.
 def move_fingers(f1=None, f2=None, f3=None, f4=None, f5=None):
     if f1 is not None:
         f1 = _clamp_percent(f1)
